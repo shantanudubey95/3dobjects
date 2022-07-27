112aad2578d1c2865c4ce09e9daa2b408b33580e
shantanudubey95 <shantanu@suggaa.in>
2022-07-27 09:14:19 +0000

    Obj image added to assets folder

--- App.tsx
+++ App.tsx
@@ -4,11 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start jjkkjjkk  </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
